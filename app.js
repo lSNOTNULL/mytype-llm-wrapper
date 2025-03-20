b7716f308b902a8c929e24a3e27161d50961000c
qus0in <qus0in@gmail.com>
2025-03-20 14:31:57 +0900

    library

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-// const express = require('express')
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
-const app = express()
-const port = 3000
-
-import dotenv from 'dotenv'
-dotenv.config()
-
 import Together from 'together-ai';
+
+const app = express();
+const port = 3000;
+
 const together = new Together();
 
-const stream = await together.chat.completions.create({
-    // https://api.together.xyz/models/meta-llama/Llama-3.3-70B-Instruct-Turbo-Free
-    // model: 'meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo',
-    model: 'meta-llama/Llama-3.3-70B-Instruct-Turbo-Free',
-    messages: [
-        { role: 'user', content: 'What are the top 3 things to do in New York?' },
-    ],
-    stream: true,
-});
-
-for await (const chunk of stream) {
-    // use process.stdout.write instead of console.log to avoid newlines
-    process.stdout.write(chunk.choices[0]?.delta?.content || '');
-}
-
-app.get('/', (req, res) => {
-    // res.send('Hello World!')
-    res.send('Good job!')
+app.get('/', async (req, res) => {
+    const result = await together.chat.completions.create({
+        // https://api.together.xyz/models/meta-llama/Llama-3.3-70B-Instruct-Turbo-Free
+        model: 'meta-llama/Llama-3.3-70B-Instruct-Turbo-Free',
+        messages: [
+            { role: 'user', content: '오늘 저녁 메뉴 추천 좀' },
+        ],
+    });
+    res.json(result.choices[0].message.content);
 })
 
 app.listen(port, () => {
